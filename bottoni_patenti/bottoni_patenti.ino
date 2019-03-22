c6786609fb0484f14ef8866327a80e01b343a0eb
riccardo <riccardo.valente@barsanti.edu.it>
2019-03-22 16:36:02 +0100

    22/03/19 conclusione del progetto

--- bottoni_patenti/bottoni_patenti.ino
+++ bottoni_patenti/bottoni_patenti.ino
@@ -5,9 +5,8 @@ int buttone = 13;
 int riflesso;
 int riflesso2;
 double wait;
-String appoggio; String appoggio2;
+String appoggio;
 LiquidCrystal lcd(0, 1,4, 5, 6, 7);
-
 void setup() {
   // put your setup code here, to run once:
  pinMode(luce, OUTPUT);
@@ -15,16 +14,42 @@ void setup() {
  pinMode(buttone, INPUT);
  lcd.begin(16, 2);
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  //per pulire dal test precedente
-   accensioneLed();
-   riflessione();
+ wait = random(1000, 5000);//faccio un random
+  delay(wait); //lo uso come delay
+  riflesso =  millis();
+  digitalWrite(luce, HIGH);//accendo il led
+   while(true)
+  {
+    if(digitalRead(buttone) == HIGH)
+    {
+      riflesso =  millis() - riflesso;
+      digitalWrite(luce, LOW);
+      break;
+    }
+  }
+  //adesso devo fare il buzzer
+ wait =  random(1000, 5000);
+  delay(wait);
+  riflesso2 =  millis();
+  tone(buzzer, 500);
+  while(true)
+  {
+    if(digitalRead(buttone) == HIGH)
+    {
+      riflesso2 = millis() -  riflesso2;
+      tone(buzzer, 500, 1);
+      break;
+    }
+  }
    lcd.setCursor(0, 0);
    lcd.print(riflesso);
    lcd.print(" ms");
-  /* if(riflesso > 500)
+      lcd.setCursor(0, 1);
+   lcd.print(riflesso2);
+   lcd.print(" ms");
+   if(riflesso > 500)
    {
     lcd.setCursor(12, 0);
     lcd.print("male");
@@ -34,9 +59,6 @@ void loop() {
     lcd.setCursor(12, 0);
     lcd.print("bene");
    }
-   lcd.setCursor(0, 1);
-   lcd.print(riflesso2);
-   lcd.print(" ms");
    if(riflesso2 > 500)
    {
     lcd.setCursor(12, 1);
@@ -46,42 +68,7 @@ void loop() {
    {
     lcd.setCursor(12, 1);
     lcd.print("bene");
-   }*/
-}
-void accensioneLed()
-{
-  wait = random(1000, 5000);//faccio un random
-  delay(wait); //lo uso come delay
-  riflesso =  millis();
-  digitalWrite(luce, HIGH);//accendo il led
-}
-void accensioneBuzzer()
-{
-  wait =  random(1000, 5000);
-  delay(wait);
-  riflesso2 =  millis();
-  tone(buzzer, 500);
-}
-void  riflessione()
-{
-  while(true)
-  {
-    riflesso =  millis() - riflesso;
-    if(digitalRead(buttone) == HIGH)
-    {
-      digitalWrite(luce, LOW);
-      break;
-    }
-  }
-  //adesso devo fare il buzzer
-  accensioneBuzzer();
-  while(true)
-  {
-    riflesso2 = millis() -  riflesso2;
-    if(digitalRead(buttone) == HIGH)
-    {
-      tone(buzzer, 500, 1);
-      break;
-    }
-  }
+   }
+   delay(2000);
+   lcd.clear();  //per pulire i risultati del test
 }
